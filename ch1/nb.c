86325d05b0887e5e2c70b4d7184e24d30526d790
Art Kamog <ia.kamog@yandex.ru>
2018-08-23 00:59:38 +0300

    exercise 1-8: nb.c

---
+++ ch1/nb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* exercise 1-8: count blanks, tabs, and newlines */
+int
+main(void)
+{
+	int c, nb, nt, nn;
+
+	nb = nt = nn = 0;
+	while ((c = getchar()) != EOF)
+		if (c == ' ')
+			++nb;
+		else if (c == '\t')
+			++nt;
+		else if (c == '\n')
+			++nn;
+	printf("%6s%6s%6s\n"
+		   "%6d%6d%6d\n",
+		   "' '", "'\\t'", "'\\n'",
+		   nb, nt, nn);
+}
