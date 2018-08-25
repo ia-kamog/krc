a1356aa7cfce0e4cae6b369894e80b529133e6b4
Art Kamog <ia.kamog@yandex.ru>
2018-08-26 02:22:38 +0300

    exercise 1-15: rewrite fahr using functions

--- ch1/fahr.c
+++ ch1/fahr.c
@@ -4,12 +4,22 @@
 #define		UPPER	300		/* upper limit */
 #define		STEP	20		/* step size */
 
+float fahr_to_cels(float fahr);
+
 /* print Fahrenheit-Celsius table */
-int
-main(void)
+int main(void)
 {
 	int fahr;
 
 	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+		printf("%3d %6.1f\n", fahr, fahr_to_cels(fahr));
+}
+
+/* fahr_to_cels:  convert fahr in Fahrenheit to correspond temperature
+	in Celsius */
+float fahr_to_cels(float fahr)
+{
+	float k = 5.0 / 9.0;
+	float zero = 32.0;
+	return k * (fahr - zero);
 }
