3800a0b1e4f93eec10f6e25b3b5192d3b1549310
Art Kamog <ia.kamog@yandex.ru>
2018-08-27 00:35:22 +0300

    longline: getline split

--- ch1/longline.c
+++ ch1/longline.c
@@ -30,21 +30,6 @@ int main(void)
 	return 0;
 }
 
-/* getline:  read a line into s, return length */
-int getline(char s[], int lim)
-{
-	int c, i;
-
-	for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
-}
-
 /* copy:  copy 'from' into 'to'; assume to is big enough */
 void copy(char to[], char from[])
 {
