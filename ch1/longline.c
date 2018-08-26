dad7ee30d99cbebc71c268da9b2ad2a18a1d8e6c
Art Kamog <ia.kamog@yandex.ru>
2018-08-26 22:27:47 +0300

    exercise 1-16

--- ch1/longline.c
+++ ch1/longline.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAXLINE 1000	/* maximum input line size */
+#define MAXLINE	1000 /* maximum input line size */
 
 int getline(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -11,15 +11,22 @@ int main(void)
 	int max;			/* maximum length seen so far */
 	char line[MAXLINE];		/* current input line */
 	char longest[MAXLINE];	/* longest line saved here */
+	int c;
 
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0)
+	while ((len = getline(line, MAXLINE)) > 0) {
+		if (len == MAXLINE-1 && line[MAXLINE-2] != '\n')
+			for (c=EOF; c!='\n' && (c=getchar())!=EOF; ++len)
+				;
 		if (len > max) {
 			max = len;
 			copy(longest, line);
 		}
+	}
 	if (max > 0)	/* there was a line */
-		printf("%s", longest);
+		printf("%d: %s", max, longest);
+	if (max >= MAXLINE-1 && longest[MAXLINE-2] != '\n')
+		putchar('\n');
 	return 0;
 }
 
