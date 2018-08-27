5e5a3d4222fd5e2444c2127f427e321ab8a0ef30
Art Kamog <ia.kamog@yandex.ru>
2018-08-28 00:10:05 +0300

    longline:  version with global variables

--- ch1/longline.c
+++ ch1/longline.c
@@ -1,41 +1,58 @@
 #include <stdio.h>
-#define MAXLINE	1000 /* maximum input line size */
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+#define MAXLINE	1000	/* maximum input line size */
 
-/* print longest input line */
+int max;				/* maximum length seen so far */
+char line[MAXLINE];		/* current input line */
+char longest[MAXLINE];	/* longest line saved here */
+
+int getline(void);
+void copy(void);
+
+
+/* print longest input line; specialized version */
 int main(void)
 {
-	int len;			/* current line length */
-	int max;			/* maximum length seen so far */
-	char line[MAXLINE];		/* current input line */
-	char longest[MAXLINE];	/* longest line saved here */
-	int c;
+	int len;
+	extern int max;
+	extern char longest[];
 
 	max = 0;
-	while ((len = getline(line, MAXLINE)) > 0) {
-		if (len == MAXLINE-1 && line[MAXLINE-2] != '\n')
-			for (c=EOF; c!='\n' && (c=getchar())!=EOF; ++len)
-				;
+	while ((len = getline()) > 0)
 		if (len > max) {
 			max = len;
-			copy(longest, line);
+			copy();
 		}
-	}
 	if (max > 0)	/* there was a line */
-		printf("%d: %s", max, longest);
-	if (max >= MAXLINE-1 && longest[MAXLINE-2] != '\n')
-		putchar('\n');
+		printf("%s", longest);
 	return 0;
 }
 
-/* copy:  copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
+
+/* getline:  specialized version */
+int getline(void)
+{
+	int c, i;
+	extern char line[];
+
+	for (i = 0; i < MAXLINE-1
+			 && (c=getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+/* copy:  specialized version */
+void copy(void)
 {
 	int i;
+	extern char line[], longest[];
 
 	i = 0;
-	while ((to[i] = from[i]) != '\0')
+	while ((longest[i] = line[i]) != '\0')
 		++i;
 }
