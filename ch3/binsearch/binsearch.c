c97b61a5ee3b6ddb3ecd4005ab7acf8dd3a16513
Art Kamog <ia.kamog@yandex.ru>
2018-09-03 02:20:02 +0300

    binsearch (exercise 3-1)

--- ch3/binsearch/binsearch.c
+++ ch3/binsearch/binsearch.c
@@ -1,18 +1,20 @@
-/* binsearch:  find x in v[0] <= v[1] <= ... <= v[n-1] */
+/* exercise 3-1
+	binsearch:  find x in v[0] <= v[1] <= ... <= v[n-1] */
 int binsearch(int x, int v[], int n)
 {
 	int low, high, mid;
 
 	low = 0;
 	high = n - 1;
-	while (low <= high) {
+	while (low < high) {
 		mid = (low+high) / 2;
 		if (x < v[mid])
 			high = mid - 1;
-		else if (x > v[mid])
-			low = mid + 1;
-		else	/* found match */
-			return mid;
+		else	
+			low = mid;
 	}
-	return -1;
+	if (v[mid] == x)
+		return mid;
+	else
+		return -1;
 }
