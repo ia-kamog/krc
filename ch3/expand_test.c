7ba7ff875cdaed98d064971fde76d0516aa6de43
Art Kamog <ia.kamog@yandex.ru>
2018-09-10 03:15:28 +0300

    expand_test:  remove newlines in input

--- ch3/expand_test.c
+++ ch3/expand_test.c
@@ -1,14 +1,17 @@
 #include <limits.h>
 #include <stdio.h>
+#include <string.h>
 
 void expand(const char s1[], char s2[]);
 
 /* test expand function */
 int main(void)
 {
-	char s1[CHAR_MAX], s2[CHAR_MAX];
+	char s1[CHAR_MAX], s2[CHAR_MAX], *p;
 	if (fgets(s1, CHAR_MAX, stdin) == NULL)
 		return 1;
+	if ((p=strrchr(s1,'\n')) != NULL)
+		*p = '\0';
 	expand(s1,s2);
 	puts(s2);
 }
