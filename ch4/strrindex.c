209f532508f6b0645c801a3dbeaf2d2abe8e7720
Art Kamog <ia.kamog@yandex.ru>
2018-09-12 03:37:37 +0300

    strrindex:  spacing normalization

--- ch4/strrindex.c
+++ ch4/strrindex.c
@@ -6,7 +6,7 @@ int strrindex(const char s[], const char t[])
 	int i, j, k;
 
 	for (i = strlen(s)-1; i >= 0; i--) {
-		for (j=0; s[i+j]!='\0'&&t[j]!='\0' && s[i+j]==t[j]; j++)
+		for (j=0; s[i+j]!='\0'&&t[j]!='\0'&&s[i+j]==t[j]; j++)
 			;
 		if (t[j] == '\0')
 			return i;
