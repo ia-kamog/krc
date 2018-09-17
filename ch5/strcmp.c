8dc0ea0a731a65eaa0eb061cc94cb81458e71d51
Art Kamog <ia.kamog@yandex.ru>
2018-09-17 18:28:01 +0300

    strcmp, pointer version

--- ch5/strcmp.c
+++ ch5/strcmp.c
@@ -1,10 +1,8 @@
 /* strcmp:  return <0 if s<t, 0 if s==t, >0 if s>t */
 int strcmp(char *s, char *t)
 {
-	int i;
-
-	for (i = 0; s[i] == t[i]; i++)
-		if (s[i] == '\0')
+	for (; *s == *t; s++, t++)
+		if (*s == '\0')
 			return 0;
-	return s[i] - t[i];
+	return *s - *t;
 }
