8177d498c98d0a2a831bc5ccf0053289a2359a26
Art Kamog <ia.kamog@yandex.ru>
2018-09-17 18:19:02 +0300

    strcpy; pointer version 3

--- ch5/strcpy.c
+++ ch5/strcpy.c
@@ -1,7 +1,7 @@
-/* strcpy:  copy t to s; pointer version 2 */
+/* strcpy:  copy t to s; pointer version 3 */
 void
 strcpy(char *s, char *t)
 {
-	while ((*s++ = *t++) != '\0') 
+	while (*s++ = *t++) 
 		;
 }
