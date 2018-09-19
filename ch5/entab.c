1ab7220f94f0de280b8ae22e4d9b933e413aefc3
Art Kamog <ia.kamog@yandex.ru>
2018-09-20 02:29:37 +0300

    entab,detab:  exercise 5-12:  -n opition makes first tabstop, +m set tab increment

--- ch5/entab.c
+++ ch5/entab.c
@@ -8,7 +8,8 @@ static int fillstops(int argc, char *argv[], int *stops);
 static void nextstop(int *prevst, int tab_incr, int **stops);
 
 /* convert spaces into tabs
-	exercise 5-11 */
+	exercise 5-11
+	exercise 5-12 */
 int
 main(int argc, char *argv[])
 {
@@ -55,17 +56,39 @@ main(int argc, char *argv[])
 static int
 fillstops(int argc, char *argv[], int *s)
 {
-	int *p, max = 0, inc;
-	
-	for (p = s; p-s<MAXTSTOPS-1 && --argc>0; p++) {
-		*p = atoi(*++argv);
-		if (*p < max) {
-			fprintf(stderr, "non-increasing list of tabstops\n");
-			exit(1);
+	char *arg;
+	int *p = s, max = 0, inc = 0, end = 0;
+		
+	while (p-s < MAXTSTOPS-2 && --argc>0) {
+		arg = *++argv;
+		switch (*arg) {
+		case '+':
+			inc = atoi(arg+1);
+			break;
+		case '-':
+			end = atoi(arg+1);
+			break;
+		default:
+			*p = atoi(arg);
+			if (*p < max) {
+				fprintf(stderr, "non-increasing list of tabstops\n");
+				exit(1);
+			}
+			max = *p;
+			p++;
+			break;
 		}
-		max = *p;
 	}
-	inc = (p==s)?TABSTOP:p[-1]-((p-1!=s)?p[-2]:0);
+	if (end && end > max)
+		*p++ = end;
+	else if (end)
+		fprintf(stderr, "bad starting tabstop %d\n", end);
+	if (!inc && p == s)
+		inc = TABSTOP;
+	else if (!inc)
+		inc = p[-1];
+	if (p-s >= 2)
+		inc -= p[-2];
 	*p = 0;
 	return inc;
 }
