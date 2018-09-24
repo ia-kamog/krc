a636dd259163f651da92bf1e244d1f82dc772bc6
Art Kamog <ia.kamog@yandex.ru>
2018-09-25 01:12:55 +0300

    lookup module

---
+++ ch6/lookup/lookup.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lookup.h"
+#define HASHSIZE 101
+
+static struct nlist *hashtab[HASHSIZE];	/* pointer table */
+
+static unsigned hash(const char *s);
+
+/* hash:  form hash value for string s */
+static unsigned
+hash(const char *s)
+{
+	unsigned hashval;
+
+	for (hashval = 0; *s != '\0'; s++)
+		hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
+}
+
+/* lookup:  look for s in hashtab */
+struct nlist *
+lookup(const char *s)
+{
+	struct nlist *np;
+
+	for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+		if (strcmp(s, np->name) == 0)
+			return np;	/* found */
+	return NULL;		/* not found */
+}
+
+/* install:  put (name, defn) in hashtab */
+struct nlist *
+install(char *name, char *defn)
+{
+	struct nlist *np;
+	unsigned hashval;
+
+	if ((np = lookup(name)) == NULL) {	/* not found */
+		np = malloc(sizeof *np);
+		if (np == NULL || (np->name = strdup(name)) == NULL)
+			return NULL;
+		hashval = hash(name);
+		np->next = hashtab[hashval];
+		hashtab[hashval] = np;
+	} else		/* already there */
+		free(np->defn);		/* free previous defn */
+	if ((np->defn = strdup(defn)) == NULL)
+		return NULL;
+	return np;
+}
---
+++ ch6/lookup/lookup.h
@@ -0,0 +1,11 @@
+#ifndef LOOKUP_H
+#define LOOKUP_H
+struct nlist {		/* table entry */
+	struct nlist *next;	/* next entry in chain */
+	char *name;			/* defined name */
+	char *defn;			/* replacement text */
+};
+
+struct nlist *lookup(const char *s);
+struct nlist *install(char *name, char *defn);
+#endif
